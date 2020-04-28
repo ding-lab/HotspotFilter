3cd4fa57608d9587bc9dcce04d39b79145167019
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-04-27 22:14:14 -0500

    Initial checkin of draft code

--- README.md
+++ README.md
@@ -12,6 +12,62 @@ Algorithm
   * Update header appropriately
 
 
+For the VCF part:
+    cat <(bedtools intersect -a varscan_snv_vcf.vcf -b test.bed) <(bedtools subtract -a varscan_snv_vcf.vcf -b test.bed) | bedtools sort -i -
+
+# Header
+
+Example header:
+```
+##FILTER=<ID=read_depth,Description="Retain calls where read depth in tumor > 14 and normal > 8. Caller = varscan ">
+```
+
+We want:
+```
+##FILTER=<ID=hotspot,Description="Retain calls: A intersect BED plus B subtract BED.  A=%s, B=%s, BED=%s">
+```
+
+If B is not provided,
+```
+##FILTER=<ID=hotspot,Description="Retain calls: A intersect BED.  A=%s, BED=%s">
+```
+
+-> should we have additional header info from VCF_A? VCF_B?  Give user option?  
+
+## Merging headers
+
+Generic filter field:
+```
+##FILTER=<ID=ID,Description="description">
+```
+
+I could add a suffix A and B to ID of every FILTER field which differs
+
+This will work to add "-A" to ID field:
+
+awk 'BEGIN{FS=",";OFS=","}{$1=$1"-A"; print}' filter.txt
+
+## common files
+    from : https://stackoverflow.com/questions/373810/unix-command-to-find-lines-common-in-two-files
+    awk 'NR==FNR{arr[$0];next} $0 in arr' file1 file2
+
+## Unique to one or the other
+
+Retain lines unique to A:
+$ awk 'FNR==NR {a[$0]++; next} !a[$0]' B.vcf A.vcf
+
+
+# Summary of merging headers
+
+A) get all lines starting with "##" common to both, write out
+B) get all lines starting with "##" which are unique to A; append `_A` to ID field, write out
+C) get all lines starting with "##" unique to B, append `_B`, write out
+D) write out header for filter
+E) write out CHROM line
+
+Then write out sorted VCF
+
+## 
 # Background reading
 
 ## GATK CombineVariants
---
+++ src/hotspot_filter.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+# Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
+# https://dinglab.wustl.edu/
+
+read -r -d '' USAGE <<'EOF'
+Retain calls from VCF_A which intersect with BED; optionally, retain calls from
+VCF_B which do not intersect with BED
+
+Usage:
+  my_script.sh [options] ARGS
+
+Options:
+-h: Print this help message
+-A VCF_A: required
+-B VCF_B: optional
+-D BED: required
+-o OUTFN: Output file.  If not specified, write to stdout
+
+Header of VCF_A and VCF_B is merged by retaining all common fields, appending _A, _B to ID field of
+FILTER lines which come from VCF_A and VCF_B, respectively.
+EOF
+
+# A) get all lines starting with "##" common to both, write out
+# B) get all lines starting with "##" which are unique to A; append `_A` to ID field, write out
+# C) get all lines starting with "##" unique to B, append `_B`, write out
+# D) write out header for filter
+# E) write out CHROM line
+# F) write out sorted VCF
+
+function confirm {
+    FN=$1
+    WARN=$2
+    if [ ! -s $FN ]; then
+        if [ -z $WARN ]; then
+            >&2 echo ERROR: $FN does not exist or is empty
+            exit 1
+        else
+            >&2 echo WARNING: $FN does not exist or is empty.  Continuing
+        fi
+    fi
+}
+
+function test_exit_status {
+    # Evaluate return value for chain of pipes; see https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
+    rcs=${PIPESTATUS[*]};
+    for rc in ${rcs}; do
+        if [[ $rc != 0 ]]; then
+            >&2 echo Fatal error.  Exiting.
+            exit $rc;
+        fi;
+    done
+}
+
+OUTFN="&1"  # output by defalt is to stdout
+# http://wiki.bash-hackers.org/howto/getopts_tutorial
+while getopts ":ho:A:B:D:" opt; do
+  case $opt in
+    h)
+      echo "$USAGE"
+      exit 0
+      ;;
+    o) 
+      OUTFN=$OPTARG
+      ;;
+    A) 
+      VCF_A=$OPTARG
+      confirm $VCF_A
+      ;;
+    B) 
+      VCF_B=$OPTARG
+      confirm $VCF_B
+      ;;
+    D) 
+      BED=$OPTARG
+      confirm $BED
+      ;;
+    \?)
+      >&2 echo "Invalid option: -$OPTARG" 
+      >&2 echo "$USAGE"
+      exit 1
+      ;;
+    :)
+      >&2 echo "Option -$OPTARG requires an argument." 
+      >&2 echo "$USAGE"
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+
+if [ "$#" -ne 0 ]; then
+    >&2 echo ERROR: Wrong number of arguments
+    >&2 echo "$USAGE"
+    exit 1
+fi
+
+if [ -z $VCF_A ]; then
+    >&2 echo ERROR: VCF_A not specifid
+    >&2 echo "$USAGE" 
+    exit 1
+fi
+if [ -z $BED ]; then
+    >&2 echo ERROR: BED not specifid
+    >&2 echo "$USAGE" 
+    exit 1
+fi
+
+# if VCF_B is not defined, then header is obtained just from $VCF_A
+# Otherwise, write out common lines, then per-VCF lines
+
+if [ ! -z $VCF_B ]; then
+
+    >&2 echo Processing VCF_A = $VCF_A
+    >&2 echo VCF_B = $VCF_B
+    >&2 echo BED = $BED
+    # First, write out common header lines
+    # from : https://stackoverflow.com/questions/373810/unix-command-to-find-lines-common-in-two-files
+    awk 'NR==FNR{arr[$0];next} $0 in arr' <( grep "^##" $VCF_A ) <( grep "^##" $VCF_B )  > $OUTFN
+
+    # Next, write out header lines unique to A, with _A appended to ID field
+    ##FILTER=<ID=ID,Description="description">
+    awk 'FNR==NR {a[$0]++; next} !a[$0]' <( grep "^##" $VCF_B ) <( grep "^##" $VCF_A ) | awk 'BEGIN{FS=",";OFS=","}{$1=$1"_A"; print}' >> $OUTFN
+
+    # Then, write out header lines unique to B, with _B appended to ID field
+    awk 'FNR==NR {a[$0]++; next} !a[$0]' <( grep "^##" $VCF_A ) <( grep "^##" $VCF_A ) | awk 'BEGIN{FS=",";OFS=","}{$1=$1"_B"; print}' >> $OUTFN
+    
+    # write out header for filter
+    printf "##FILTER=<ID=hotspot,Description=\"Retain calls: A intersect BED plus B subtract BED.  A=%s, B=%s, BED=%s\">" "$VCF_A" "$VCF_B" "$BED" >> $OUTFN
+
+    # Finally, write out CHROM header line
+    grep "^#CHROM" $VCF_A >> $OUTFN
+
+    # now write out merged sorted VCF
+    cat <(bedtools intersect -a $VCF_A -b $BED) <(bedtools subtract -a $VCF_B -b $BED) | bedtools sort -i - >> $OUTFN
+
+else
+    >&2 echo Processing VCF_A = $VCF_A
+    >&2 echo BED = $BED
+    >&2 echo VCF_B not provided
+
+    # VCF_B is not specified.  Just write header from VCF_A, add out filter line, and VCF_A which intersects with BED
+    grep "^##" $VCF_A > $OUT_FN
+
+    printf "##FILTER=<ID=hotspot,Description=\"Retain calls: A intersect BED.  A=%s, BED=%s\">" "$VCF_A" "$BED" >> $OUTFN
+    grep "^#CHROM" $VCF_A >> $OUTFN
+
+    bedtools intersect -a $VCF_A -b $BED >> $OUTFN
+fi
+
+>&2 echo Written to $OUTFN
+     
