8ff4ef1f4861476ffa6336b9eddd87406d0d6867
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-04-28 16:40:41 -0500

    Documentation updates

--- testing/direct_call/2_run_direct.VCF_A.sh
+++ testing/direct_call/2_run_direct.VCF_A.sh
@@ -1,6 +1,9 @@
 # Example command to run within docker.  Typically, start docker first with 0_start_docker.sh
 
-VCF_A="/data/varscan_snv_vcf.vcf"
+# Variant - VCF_B is not specified.
+# Retain VCF_A variants within BED
+
+VCF_A="/data/VCF_A.varscan_snv_vcf.vcf"
 BED="/data/test.bed"
 OUTD="./out"
 mkdir -p $OUTD
